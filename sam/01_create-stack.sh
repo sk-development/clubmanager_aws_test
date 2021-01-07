b818fb615dec8a280b2a5f3801adb002d9a7d0bf
steve <kraus.stefan@gmx.de>
2021-01-07 15:00:51 +0100

    sam template file renamed

--- sam/01_create-stack.sh
+++ sam/01_create-stack.sh
@@ -2,12 +2,12 @@
 source 00_properties.sh
 
 aws cloudformation package \
-         --template-file sam-template.yaml \
-         --output-template-file sam-template.packaged.yaml \
+         --template-file template.yaml \
+         --output-template-file template.packaged.yaml \
          --s3-bucket cf-templates-1cyvm6msnefyy-eu-central-1
 
 aws cloudformation deploy \
         --capabilities CAPABILITY_IAM \
-         --template-file sam-template.packaged.yaml \
+         --template-file template.packaged.yaml \
          --stack-name $STACKNAME \
          --parameter-overrides LambdaExecutionRole=$LAMBDA_EXECUTION_ROLE
