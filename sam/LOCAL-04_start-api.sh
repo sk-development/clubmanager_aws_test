3899df7f0c4d136ab0896edef8d6d81a847e33b5
Stefan Kraus <kraus.stefan@gmx.de>
2021-04-05 13:52:33 +0200

    Launch configs for sam local api debugging

--- sam/LOCAL-04_start-api.sh
+++ sam/LOCAL-04_start-api.sh
@@ -5,3 +5,8 @@ sam local start-api \
     --template-file template.yaml \
     --docker-network localstack_default \
     --parameter-overrides LocalEndpoint=$LOCAL_ENDPOINT
+
+# sam local start-api -d 5858 \
+#     --template-file template.yaml \
+#     --docker-network localstack_default \
+#     --parameter-overrides LocalEndpoint=$LOCAL_ENDPOINT
