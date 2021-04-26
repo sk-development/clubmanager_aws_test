d6b7f7340f199a4175a013c0ee687d59d2f13d2e
simcod <49491825+simcod@users.noreply.github.com>
2021-04-26 21:46:33 +0200

    workload delete + update lambda

--- sam/LOCAL-06_invoke-deleteSurvey.sh
+++ sam/LOCAL-06_invoke-deleteSurvey.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 source 00_properties.sh
 
+# for debugging
+# sam local invoke "DeleteSurveys" -d 5858 -e events/deleteSurveys.json \
+#     --docker-network localstack_default \
+#     --parameter-overrides LocalEndpoint=$LOCAL_ENDPOINT
+
 sam local invoke "DeleteSurveys" -e events/deleteSurveys.json \
     --docker-network localstack_default \
     --parameter-overrides LocalEndpoint=$LOCAL_ENDPOINT
--- sam/src/layers/aws-integration/survey-repository.js
+++ sam/src/layers/aws-integration/survey-repository.js
@@ -1,5 +1,6 @@
 var dynamoDbCommon = require('./dynamoDb-common');
 var dynamoDb = dynamoDbCommon.getDynamoDb();
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
 async function getSurveys() {
     const retData = [];
@@ -8,16 +9,65 @@ async function getSurveys() {
     };
 
     const data = await dynamoDb.scan(params).promise();
-    for (var i = 0; i < data.Items.length; i++) {
-        var item = data.Items[i]
-        retData.push({
-            id: item.id.S,
-            title: item.name.S
+    // for (var i = 0; i < data.Items.length; i++) {
+    //     var item = data.Items[i]
+    //     retData.push({
+    //         id: item.id.S,
+    //         title: item.title.S,
+    //         validTo: item.validTo.S,
+    //         description: item.description.S,
+    //         options: item.options.L
+    //     })
+    // }
+    // return retData;
+    return data;
+}
+
+async function updateSurvey(event) {
+    const data = JSON.parse(event.body)
+    var params = {
+        TableName: process.env.TABLE_NAME,
+        Key: marshall({
+            id: data.id
+        }),
+        UpdateExpression: "set title = :t, validTo=:v, description=:d, options=:o",
+        ExpressionAttributeValues: marshall({
+            ":t": data.title,
+            ":v": data.validTo,
+            ":d": data.description,
+            ":o": data.options
+        }),
+    }
+    var result;
+    try {
+        const item = await dynamoDb.updateItem(params).promise();
+        result = 'Success'
+    } catch (err) {
+        result = err;
+    }
+    return result;
+}
+
+async function deleteSurvey(event) {
+    const data = JSON.parse(event.body)
+    var params = {
+        TableName: process.env.TABLE_NAME,
+        Key: marshall({
+            id: data.id
         })
     }
-    return retData;
+    var result;
+    try {
+        const item = await dynamoDb.deleteItem(params).promise();
+        result = 'Success'
+    } catch (err) {
+        result = err;
+    }
+    return result;
 }
 
 module.exports = {
     getSurveys: getSurveys,
+    updateSurvey: updateSurvey,
+    deleteSurvey: deleteSurvey
 };
