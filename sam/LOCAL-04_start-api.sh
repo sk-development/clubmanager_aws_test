34baaa2f5678bdf1b5a69201f30214d3c3131372
simcod <49491825+simcod@users.noreply.github.com>
2021-05-21 19:30:17 +0200

    make containers persisten after first invokation

--- sam/LOCAL-04_start-api.sh
+++ sam/LOCAL-04_start-api.sh
@@ -4,7 +4,10 @@ source 00_properties.sh
 sam local start-api \
     --template-file template.yaml \
     --docker-network localstack_default \
-    --parameter-overrides LocalEndpoint=$LOCAL_ENDPOINT
+    --parameter-overrides LocalEndpoint=$LOCAL_ENDPOINT \
+   --warm-containers LAZY
+
+    # --warm-containers EAGER
 
 # sam local start-api -d 5858 \
 #     --template-file template.yaml \
