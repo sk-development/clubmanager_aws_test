7f818285f5c42153a2ef5a35cf386dc568b5fbc4
simcod <49491825+simcod@users.noreply.github.com>
2021-08-12 10:35:45 +0200

    update

--- sam/00_properties_TEMPLATE.sh
+++ sam/00_properties_TEMPLATE.sh
@@ -14,6 +14,8 @@ STACKNAME=<Name for the stack> # HINT: Only chars/numbers and "-" allowed
 
 # Lambda properties
 LAMBDA_EXECUTION_ROLE=arn:aws:iam::$ACCOUNT_ID:role/SK_LambdaBasicExecutionRole_test
+AUTH_HOST=<>
+AUTH_TOKEN=<>
 
 # DynamoDB properties
 TABLE_NAME_SURVEYS_GENERATED='PUT THE NAME OF THE GENERATED SURVEYS TABLE HERE'
